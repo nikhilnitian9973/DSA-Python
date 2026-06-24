5ec74e2920d095e0d1158ed338611be99b851a38
Nikhil <nikhilnitian99@gmail.com>
2026-06-24 17:43:24 +0530

    i had not saved this file before

---
+++ Bit_Manipulation/swap_two_numbers.py
@@ -0,0 +1,29 @@
+#using XOR
+
+a, b = 5 ,6
+
+a = a^b
+b= a^b
+a=a^b
+
+#using + and - operators
+
+a = a+b
+b = a-b
+a = a-b
+
+#using * and / operators
+
+a = a*b
+b = a/b
+a = a/b
+
+#using python trick
+
+a,b = b,a
+
+#using third variable
+
+c = a
+a = b
+b = c
